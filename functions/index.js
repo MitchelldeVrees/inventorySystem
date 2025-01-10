348cbb84ad1c952a2666f1baf123c969286b49b0
Mitchell de Vries <mitchelldevries2001@gmail.com>
2025-01-10 13:46:06 +0100

    console loggin

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
   const pathParts = url.pathname.split('/').filter(Boolean); // Split path into parts
-  
+  console.log(url)
   // Route to `/api/products` or `/api/products/:id` handlers
   if (pathParts[0] === 'api' && pathParts[1] === 'products') {
     // Dynamically import the products.js file and call its handler
